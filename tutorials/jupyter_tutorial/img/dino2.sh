a46a67195f2963f8b99cbe8452b5399697d0f31b
lingchen42 <ling.chen@vanderbilt.edu>
2018-05-01 17:46:11 -0500

    jupyter notebook tutorial added 2018-05-01

---
+++ tutorials/jupyter_tutorial/img/dino2.sh
@@ -0,0 +1,8 @@
+printf "\n\n\n"
+printf "                                   __  \n"
+printf "                                  / _)  "
+printf "                           .-^^^-/ /  \n"
+printf "                        __/       /  \n"
+printf "                       <__.|_|-|_|  \n"
+printf "\n\n\t\t\t"
+printf "\n\n"
